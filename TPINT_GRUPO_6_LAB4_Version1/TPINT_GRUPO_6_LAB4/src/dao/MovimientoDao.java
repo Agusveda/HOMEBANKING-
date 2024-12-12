ef6f44bd070c45e76db396874005105a992c9bcf
Agustin Herrera <agusveda110303@gmail.com>
2024-12-12 17:38:14 -0300

    a

--- TPINT_GRUPO_6_LAB4_Version1/TPINT_GRUPO_6_LAB4/src/dao/MovimientoDao.java
+++ TPINT_GRUPO_6_LAB4_Version1/TPINT_GRUPO_6_LAB4/src/dao/MovimientoDao.java
@@ -11,27 +11,38 @@ import Entidades.Prestamo;
 
 public interface MovimientoDao 
 {
-	int ObtenerIdCuentaPorCBU(int CBU);
-	boolean insertar(Movimiento movi, int idCue);
-	boolean insertarAltaCuenta(Movimiento movi, int idCue);
-	int ObtenerIdCuentaPorIdCliente(int IdCliente); 
-	ArrayList<Cuenta> TraeCuentasPorIdCliente(int idCliente);
-	float ObtenerSaldoPorIdCuenta(int idCue); 
-	ArrayList<Movimiento> ListarMovimientosPorCuenta(int idCue);
-	boolean ExisteCBU(int Cbu);
-    boolean insertarPrestamo(Prestamo prestamo);
-    ArrayList<Prestamo> ListPrestamosPedidos();
-    boolean actualizarConfirmacionPrestamo(int idPrestamo, int confirmacion);
-    ArrayList<Prestamo> ListPrestamosPedidosAutorizados();
-    ArrayList<Prestamo> filtrarClienteXImporte (String orden); 
-    ArrayList<Prestamo> filtrarClienteXImporteConfirmado (String orden);     
-    boolean CargarPrestamoEnCuenta(int idcuenta, float monto);
-    double obtenerTotalPrestamosConfirmados(int idCliente);
+	public int ObtenerIdCuentaPorCBU(int CBU);
+	public boolean insertar(Movimiento movi, int idCue);
+	public boolean insertarAltaCuenta(Movimiento movi, int idCue);
+	public int ObtenerIdCuentaPorIdCliente(int IdCliente); 
+	public ArrayList<Cuenta> TraeCuentasPorIdCliente(int idCliente);
+	public float ObtenerSaldoPorIdCuenta(int idCue); 
+	public ArrayList<Movimiento> ListarMovimientosPorCuenta(int idCue);
+	public boolean ExisteCBU(int Cbu);
+	
+	///PRESTAMOS
+    public boolean insertarPrestamo(Prestamo prestamo);
+    public ArrayList<Prestamo> ListPrestamosPedidos();
+    public boolean actualizarConfirmacionPrestamo(int idPrestamo, int confirmacion);
+    public ArrayList<Prestamo> ListPrestamosPedidosAutorizados();
+    public ArrayList<Prestamo> filtrarClienteXImporte (String orden); 
+    public ArrayList<Prestamo> filtrarClienteXImporteConfirmado (String orden);     
+    public boolean CargarPrestamoEnCuenta(int idcuenta, float monto);
+    public double obtenerTotalPrestamosConfirmados(int idCliente);
+    
 	List<Prestamo> obtenerPrestamosConfirmados(int idCliente);
-	List<Cuota> obtenerCuotas(int idCliente, int idPrestamo);
-	boolean realizarPagoCuota(int cuotaId, int cuentaId, float monto);
-	double obtenerSumaCuotasPendientes(int idCliente);
-    float ReporteMovimiento(int TipoMovimiento, String FechaInicio, String FechaFinal); 
-    float EgresoDeCliente(int DNICLIENTE);
-    float IngresoDeCliente(int DNICLIENTE);
+	public List<Cuota> obtenerCuotas(int idCliente, int idPrestamo);
+	public boolean realizarPagoCuota(int cuotaId, int cuentaId, float monto);
+	public double obtenerSumaCuotasPendientes(int idCliente);
+
+	
+	
+    ///REPORTE DE MOVIMIENTO
+    public float ReporteMovimiento(int TipoMovimiento, String FechaInicio, String FechaFinal); 
+    
+    // REPORTE DE TRANSFERENCIAS
+    public float EgresoDeCliente(int DNICLIENTE);
+    public float IngresoDeCliente(int DNICLIENTE);
+    
+
 }
